72e912b56cb023e30c5c9963977949fd1985f952
孙立中 <sunlizhong@zhidaoauto.com>
2020-01-07 00:29:42 +0800

    2020年1月6日

--- src/main/java/easy/_107二叉树的层次遍历2.java
+++ src/main/java/easy/_107二叉树的层次遍历2.java
@@ -1,6 +1,6 @@
 package easy;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
@@ -29,16 +29,62 @@ public class _107二叉树的层次遍历2 {
 
     public static void main(String[] args) {
 
+        TreeNode head = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode four = new TreeNode(4);
+        TreeNode five = new TreeNode(5);
+        TreeNode six = new TreeNode(6);
+        TreeNode seven = new TreeNode(7);
+        head.right = three;
+        head.left = second;
+        second.right = five;
+        second.left = four;
+        three.right = seven;
+        three.left = six;
+
+
+        levelOrderBottom(head);
+
 
     }
 
 
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
 
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        Map<Integer, List<Integer>> result = new HashMap<>();
+        levelOrderBottom(root, 0, result);
+
+        List<List<Integer>> lists = new ArrayList<>();
+        result.forEach(lists::add);
+        Collections.reverse(lists);
+
+        return lists;
+    }
+
+    public static void levelOrderBottom(TreeNode root, Integer level, Map<Integer, List<Integer>> result) {
+
+
+        List<Integer> curLevelNode = result.getOrDefault(level, new ArrayList<>());
+        curLevelNode.add(root.val);
+        result.put(level, curLevelNode);
+
+
+        if (root.left != null) {
+            levelOrderBottom(root.left, level + 1, result);
+        }
+
+        if (root.right != null) {
+            levelOrderBottom(root.right, level + 1, result);
+        }
     }
 
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;

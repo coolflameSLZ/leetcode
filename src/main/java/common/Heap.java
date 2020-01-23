1dd363250133cb6a9da3f83c9c30df7a9b6a7574
孙立中 <sunlizhong@zhidaoauto.com>
2020-01-23 23:39:32 +0800

    2020年1月23日

--- src/main/java/common/Heap.java
+++ src/main/java/common/Heap.java
@@ -4,6 +4,7 @@ package common;
  * 堆
  */
 public class Heap {
+    public static final int HEAP_BEGIN_IDX = 1;
     private int[] heapArray; // 数组，从下标1开始存储数据
     private int capacity; // 堆可以存储的最大数据个数
     private int count; // 堆中已经存储的数据个数
@@ -14,18 +15,6 @@ public class Heap {
         count = 0;
     }
 
-
-    /**
-     * 构建堆
-     *
-     * @param a
-     * @param n
-     */
-    private static void buildHeap(int[] a, int n) {
-        for (int i = n / 2; i >= 1; --i) {
-            heapify(a, n, i);
-        }
-    }
 
     /**
      * 插入 ，
@@ -40,11 +29,12 @@ public class Heap {
             throw new RuntimeException("堆满了");
         }
 
-        // 1
+        // 1 把数据追加到数组最后
         count++;
         heapArray[count] = data;
 
-        // 2 执行条件，孩子比父亲大
+        // 2 如果最后追加的这个元素，比他的父亲大
+        // 就进行向上堆化
         int i = this.count;
         while (getParentIndex(i) > 0 && heapArray[i] > heapArray[getParentIndex(i)]) {
 
@@ -56,64 +46,120 @@ public class Heap {
 
     /**
      * 删除最大元素
+     *
+     * @return 删除的最大值
      */
-    public void removeMax() {
+    public int removeMax() {
 
         if (count == 0) {
             throw new RuntimeException("堆中没有元素");
         }
 
+        int result = heapArray[HEAP_BEGIN_IDX];
+
         count--;
         // 数组最后一位放到堆顶
-        heapArray[1] = heapArray[count];
+        heapArray[HEAP_BEGIN_IDX] = heapArray[count];
         // 堆化
-        heapify(heapArray, 1, count);
+        heapifyDown(heapArray, 1, count);
+
+        return result;
+    }
+
+
+    /**
+     * 将一个数组完全堆化，
+     * 从最后一个元素的父亲开始，依次向前堆化。
+     * 直到head完成堆化
+     *
+     * @param array
+     * @param heapSize
+     */
+    public static void heapifyAllArray(int[] array, int heapSize) {
+        for (int i = getParentIndex(heapSize); i > 0; --i) {
+            heapifyDown(array, i, heapSize);
+        }
     }
 
     /**
-     * 堆化， 将一个数组，变成大顶堆
+     * 自上向下堆化。
+     * 从头节点开始，与更大的儿子进行交换。 然后将指针指向父亲交换后的位置,
      *
      * @param heapArray
-     * @param head
-     * @param tail
+     * @param beginNode
+     * @param heapSize
      */
-    public static void heapify(int[] heapArray, int head, int tail) {
+    public static void heapifyDown(int[] heapArray, int beginNode, int heapSize) {
+
+        int index = beginNode;
 
         while (true) {
-            int maxPos = head;
+            int maxPos = index;
 
-            if (getLeftSonIndex(head) <= tail && heapArray[head] < heapArray[getLeftSonIndex(head)]) {
-                maxPos = getLeftSonIndex(head);
+            // 父亲比左儿子小，最大值换成左儿子
+            if (getLeftSonIndex(beginNode) <= heapSize && heapArray[beginNode] < heapArray[getLeftSonIndex(beginNode)]) {
+                maxPos = getLeftSonIndex(beginNode);
             }
 
-            if (getRightSonIndex(head) <= tail && heapArray[head] < heapArray[getRightSonIndex(head)]) {
-                maxPos = getRightSonIndex(head);
+            // 如果右儿子更大，那么最大值换成右儿子
+            if (getRightSonIndex(beginNode) <= heapSize && heapArray[maxPos] < heapArray[getRightSonIndex(beginNode)]) {
+                maxPos = getRightSonIndex(beginNode);
             }
 
-            if (maxPos == head) {
+            // 如果 经过了两轮比较，父亲依然是最大的，那么退出
+            if (maxPos == beginNode) {
                 break;
-            }
 
-            swap(heapArray, tail, maxPos);
-            head = maxPos;
+                // 交换后，指针指向交换后的位置（新的最大值）
+            } else {
+                swap(heapArray, beginNode, maxPos);
+                index = maxPos;
+            }
         }
 
 
     }
 
+    /**
+     * 自下向上的堆化
+     * <p>
+     * 从给定的节点开始，
+     * 如果他比父亲大，就与父亲交换，刷新指针的位置，指向交换后的位置
+     *
+     * @param heapArray
+     * @param tail
+     */
+    public static void heapifyUp(int[] heapArray, int tail) {
+
+        int index = tail;
+
+        while (heapArray[index] > heapArray[getParentIndex(index)]) {
+            swap(heapArray, index, getParentIndex(index));
+            index = getParentIndex(index);
+        }
+
+
+    }
+
+
     //========似有方法============
 
     /**
-     * 交换
+     * 交换数组中两个位置的值
      *
-     * @param heapArray
-     * @param i
-     * @param j
+     * @param array  数组
+     * @param index1 位置1
+     * @param index2 位置2
+     * @return 交换后的数组，与传入数组为同一对象
      */
-    private static void swap(int[] heapArray, int i, int j) {
-        int tmp = heapArray[i];
-        heapArray[i] = heapArray[j];
-        heapArray[j] = tmp;
+    public static int[] swap(int[] array, int index1, int index2) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("Number array must not empty !");
+        }
+        int tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
+        return array;
     }
 
     /**
@@ -122,7 +168,7 @@ public class Heap {
      * @param index
      * @return
      */
-    private int getParentIndex(int index) {
+    public static int getParentIndex(int index) {
         return index / 2;
     }
 
@@ -132,7 +178,7 @@ public class Heap {
      * @param index
      * @return
      */
-    private static int getLeftSonIndex(int index) {
+    public static int getLeftSonIndex(int index) {
         return index * 2;
     }
 
@@ -142,7 +188,7 @@ public class Heap {
      * @param index
      * @return
      */
-    private static int getRightSonIndex(int index) {
+    public static int getRightSonIndex(int index) {
         return (index * 2) + 1;
     }
 
